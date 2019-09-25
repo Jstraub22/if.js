b59cc4050b0eaa93e63c9e26f44a2aca64195ed2
Jstraub22 <54632666+Jstraub22@users.noreply.github.com>
2019-09-25 10:48:35 -0400

    Add files via upload

---
+++ if.js
@@ -0,0 +1,35 @@
+var likestotravel = true;
+if(likestotravel === true){
+    console.log("Bon Voyage!");
+  } else {
+    console.log("Enjoy your couch.")
+
+
+
+  }
+  var time ="afternoon";
+
+  if (time === "morning"){
+    console.log("Good morning!");
+  } else if (time === "afternoon"){
+    console.log("Good afternoon!");
+  } else if (time === "evening"){
+    console.log("Good evening!");
+  } else {
+    console.log("Hello!");
+  }
+
+var likesdogs = true;
+if(likesdogs === true){
+    console.log("congrats");
+
+} else {
+    console.log("dogs")
+}
+
+var birthyear = 1999;
+
+if (birthyear === true){
+    console.log("GEN Z");
+
+} else { console.log("1999")}
